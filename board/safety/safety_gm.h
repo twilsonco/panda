bc3e87ddab72382e803bee8954b68b745f81738f
Tim Wilson <twilsonco@gmail.com>
2021-10-15 16:58:10 -0600

    apply jyoung acadia tune https://github.com/commaai/panda/commit/f5277b88e23313ae864c8cfff40ab5dc31ac5c23

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -9,10 +9,10 @@
 //      brake > 0mph
 
 const int GM_MAX_STEER = 300;
-const int GM_MAX_RT_DELTA = 128;          // max delta torque allowed for real time checks
+const int GM_MAX_RT_DELTA = 75;          // 4 max rate up * 50Hz send rate * 250000 RT interval / 1000000 = 50 ; 50 * 1.5 for safety pad = 75
 const uint32_t GM_RT_INTERVAL = 250000;    // 250ms between real time checks
-const int GM_MAX_RATE_UP = 7;
-const int GM_MAX_RATE_DOWN = 17;
+const int GM_MAX_RATE_UP = 8;
+const int GM_MAX_RATE_DOWN = 20;
 const int GM_DRIVER_TORQUE_ALLOWANCE = 50;
 const int GM_DRIVER_TORQUE_FACTOR = 4;
 const int GM_MAX_GAS = 3072;
